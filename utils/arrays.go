08c6effe1951c2f1c1e115936bdecbcc74024517
Ashwitha <ashwitha@Ashwithas-MacBook-Pro.local>
2021-12-07 21:58:15 +0530

    Adds helper functions

---
+++ utils/arrays.go
@@ -0,0 +1,6 @@
+package utils
+
+func GetMidElement(elements []int) int {
+	midIndex := len(elements) / 2
+	return elements[midIndex]
+}
--- utils/helper.go
+++ utils/helper.go
@@ -1,5 +1,7 @@
 package utils
 
+import "strconv"
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
@@ -7,3 +9,28 @@ func Abs(x int) int {
 	return x
 }
 
+func toInt(value string) int {
+	number, err := strconv.Atoi(value)
+	if err!=nil {
+		panic(err)
+	}
+	return number
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Min(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func SummationOfNaturalNumbers(numberOfElements int) int {
+	return numberOfElements * (numberOfElements + 1) / 2
+}
